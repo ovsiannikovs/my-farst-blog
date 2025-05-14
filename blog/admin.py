1d6af19c4a997fab3f3faead9c48ff7d116f934b
Your Name <you@example.com>
2025-05-14 13:20:28 +0300

    Добавлена модель TechnicalProposal и миграции

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from .models import TechnicalProposal
+
+admin.register(TechnicalProposal)
+class TechnicalProposalAdmin(admin.ModelAdmin):
+    list_display = ['name', 'author', 'date_of_creation']
+
 
 admin.site.register(Post)
 class PostAdmin(admin.ModelAdmin):
