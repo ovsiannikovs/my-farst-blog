7e934b5bce6e34df96f89b0e2377593fa56df8fb
ovsiannikovs <seva.ovsyannikov@mail.ru>
2025-05-21 16:48:55 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bt-j2-rr@ps7e(_mg*&=c=cux-z)l9n-p(676=xfdo^pv1vj6s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '188.120.244.46', 'spmlc.ru']
 
