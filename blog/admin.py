564ccf4a28f1719fbedb2798ce28b72b6c0a6645
Your Name <you@example.com>
2025-07-16 13:26:50 +0300

    Обновление admin.py: добавлены модели и конфигурация

--- blog/admin.py
+++ blog/admin.py
@@ -10,12 +10,20 @@ from .models import GeneralDrawingUnit
 from .models import ElectronicModelUnit
 from .models import DrawingPartUnit
 from .models import ElectronicModelPartUnit
-from .models import DrawingPartProduct
+from .models import DrawingPartProduct 
 from .models import ElectronicModelPartProduct
 from .models import ReportTechnicalProposal
 from .models import AddReportTechnicalProposal
 from .models import ProtocolTechnicalProposal
-from crm.models import Notifications, Customer, Decision_maker, Deal, Product, Deal_stage, Call, Letter
+from .models import TechTask
+from .models import (
+    OKRTask,
+    Template,
+    ReworkTask,
+    WorkPlan,
+)
+
+
 
 
 @admin.register(TechnicalProposal)
@@ -23,11 +31,23 @@ class TechnicalProposalAdmin(admin.ModelAdmin):
     list_display = ['name', 'author', 'date_of_creation']
     readonly_fields = ('date_of_creation', 'date_of_change')
 
+class ListTechnicalProposalInline(admin.TabularInline):
+    model = ListTechnicalProposal
+    extra = 1
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('name', 'design_product', 'author', 'date_of_creation', 'date_of_change')
+    list_display = ('name', 'desig_product', 'author', 'date_of_creation', 'date_of_change')
+    inlines = [ListTechnicalProposalInline]
 
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in instances:
+            instance.post = form.instance
+            if not instance.name:
+                instance.name = instance.post.name
+            instance.save()
+        formset.save_m2m()
 
 @admin.register(ListTechnicalProposal)
 class ListTechnicalProposalAdmin(admin.ModelAdmin):
@@ -35,49 +55,48 @@ class ListTechnicalProposalAdmin(admin.ModelAdmin):
     search_fields = ['name', 'desig_document']
 
 
+    def save_model(self, request, obj, form, change):
+        if obj.post and not obj.name:
+            obj.name = obj.post.name
+        super().save_model(request, obj, form, change)
+
 @admin.register(GeneralDrawingProduct)
 class GeneralDrawingProductAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 'desig_document', 'category', 'author', 'date_of_creation', 'status', 'version',
+        'name','desig_document','category','author','date_of_creation','status','version',
     )
     search_fields = ('name', 'desig_document')
     list_filter = ('category', 'status', 'trl', 'litera')
 
-
 @admin.register(ElectronicModelProduct)
 class ElectronicModelProductAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 'desig_document', 'author', 'date_of_creation', 'status', 'version', 'trl',
+        'name','desig_document','author','date_of_creation','status','version','trl',
     )
     search_fields = ('name', 'desig_document')
     list_filter = ('status', 'trl', 'category', 'develop_org')
 
-
 @admin.register(GeneralElectricalDiagram)
 class GeneralElectricalDiagramAdmin(admin.ModelAdmin):
     list_display = (
-        'name', 'desig_document', 'author', 'date_of_creation', 'status', 'version',
+        'name','desig_document','author','date_of_creation','status','version',
     )
     search_fields = ('name', 'desig_document', 'author__username')
-    list_filter = ('status', 'trl', 'develop_org', 'language')
-
+    list_filter = ('status', 'trl', 'develop_org', 'language')    
 
 @admin.register(SoftwareProduct)
 class SoftwareProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'category', 'desig_document', 'status', 'version', 'date_of_creation')
     search_fields = ('name', 'desig_document', 'status')
-    list_filter = ('status', 'category', 'trl', 'version')
-
+    list_filter = ('status', 'category', 'trl', 'version')    
 
 @admin.register(GeneralDrawingUnit)
 class GeneralDrawingUnitAdmin(admin.ModelAdmin):
-    list_display = ('name', 'desig_document', 'status', 'version', 'technical_proposal')
-
+    list_display = ('name', 'desig_document', 'status', 'version')    
 
 @admin.register(ElectronicModelUnit)
 class ElectronicModelUnitAdmin(admin.ModelAdmin):
-    list_display = ('name', 'desig_document', 'status', 'version', 'technical_proposal')
-
+    list_display = ('name', 'desig_document', 'status', 'version')
 
 @admin.register(DrawingPartUnit)
 class DrawingPartUnitAdmin(admin.ModelAdmin):
@@ -100,13 +119,13 @@ class DrawingPartUnitAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {
             'fields': (
-                'name', 'category', 'desig_document', 'technical_proposal',
-                'file', 'application', 'info_format', 'primary_use', 'change_number'
+                'name', 'category', 'desig_document',
+                'info_format', 'primary_use', 'change_number'
             )
         }),
         ('Состояние и управление', {
             'fields': (
-                'status', 'priority', 'approval_cycle', 'version', 'version_diff',
+                'status', 'priority', 'version', 'version_diff',
                 'litera', 'trl', 'validity_date', 'subscribers', 'related_documents'
             )
         }),
@@ -120,8 +139,7 @@ class DrawingPartUnitAdmin(admin.ModelAdmin):
                 'date_of_creation', 'date_of_change', 'pattern'
             )
         }),
-    )
-
+    )  
 
 @admin.register(ElectronicModelPartUnit)
 class ElectronicModelPartUnitAdmin(admin.ModelAdmin):
@@ -144,9 +162,9 @@ class ElectronicModelPartUnitAdmin(admin.ModelAdmin):
         (None, {
             'fields': (
                 'category', 'name', 'desig_document', 'info_format',
-                'primary_use', 'change_number', 'file', 'application',
-                'technical_proposal', 'pattern', 'version', 'version_diff',
-                'approval_cycle', 'litera', 'trl', 'validity_date',
+                'primary_use', 'change_number',
+                'pattern', 'version', 'version_diff',
+                'litera', 'trl', 'validity_date',
                 'subscribers', 'related_documents', 'develop_org', 'language'
             )
         }),
@@ -159,8 +177,7 @@ class ElectronicModelPartUnitAdmin(admin.ModelAdmin):
         ('Временные метки', {
             'fields': ('date_of_creation', 'date_of_change')
         }),
-    )
-
+    )   
 
 @admin.register(DrawingPartProduct)
 class DrawingPartProductAdmin(admin.ModelAdmin):
@@ -185,8 +202,7 @@ class DrawingPartProductAdmin(admin.ModelAdmin):
         if not change:
             obj.author = request.user
         obj.last_editor = request.user
-        super().save_model(request, obj, form, change)
-
+        super().save_model(request, obj, form, change) 
 
 @admin.register(ElectronicModelPartProduct)
 class ElectronicModelPartProductAdmin(admin.ModelAdmin):
@@ -203,8 +219,7 @@ class ElectronicModelPartProductAdmin(admin.ModelAdmin):
         if not obj.pk:
             obj.author = request.user
         obj.last_editor = request.user
-        super().save_model(request, obj, form, change)
-
+        super().save_model(request, obj, form, change)          
 
 @admin.register(ReportTechnicalProposal)
 class ReportTechnicalProposalAdmin(admin.ModelAdmin):
@@ -218,11 +233,9 @@ class ReportTechnicalProposalAdmin(admin.ModelAdmin):
         'date_of_creation', 'date_of_change', 'author', 'last_editor'
     )
 
-
 @admin.register(AddReportTechnicalProposal)
 class AddReportTechnicalProposalAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
         'name',
         'category',
         'status',
@@ -234,21 +247,15 @@ class AddReportTechnicalProposalAdmin(admin.ModelAdmin):
         'date_of_change',
     )
     list_filter = ('category', 'status', 'trl', 'date_of_creation')
-    search_fields = ('name', 'desig_document', 'author__username', 'current_responsible__username')
+    search_fields = ('name', 'author__username', 'current_responsible__username')
     readonly_fields = ('date_of_creation', 'date_of_change', 'author', 'last_editor')
     fieldsets = (
         (None, {
             'fields': (
                 'category',
                 'name',
-                'desig_document',
                 'info_format',
-                'primary_use',
-                'file',
-                'application',
                 'status',
-                'priority',
-                'approval_cycle',
                 'version',
                 'version_diff',
                 'litera',
@@ -256,11 +263,8 @@ class AddReportTechnicalProposalAdmin(admin.ModelAdmin):
                 'validity_date',
                 'subscribers',
                 'related_documents',
-                'pattern',
                 'develop_org',
                 'language',
-                'permission',
-                'access_level',
                 'author',
                 'last_editor',
                 'current_responsible',
@@ -268,8 +272,7 @@ class AddReportTechnicalProposalAdmin(admin.ModelAdmin):
                 'date_of_change',
             )
         }),
-    )
-
+    )   
 
 @admin.register(ProtocolTechnicalProposal)
 class ProtocolTechnicalProposalAdmin(admin.ModelAdmin):
@@ -288,89 +291,63 @@ class ProtocolTechnicalProposalAdmin(admin.ModelAdmin):
         if not obj.pk:
             obj.author = request.user
         obj.last_editor = request.user
-        super().save_model(request, obj, form, change)
+        super().save_model(request, obj, form, change)     
+
+@admin.register(OKRTask)
+class OKRTaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version', 'status', 'created_by', 'created_at', 'updated_at')
+    search_fields = ('name', 'version')
+    list_filter = ('status', 'priority', 'category')
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
 
 
-class RevenueRangeFilter(admin.SimpleListFilter):
-    title = 'Выручка'
-    parameter_name = 'revenue_range'
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version', 'template_type', 'status', 'created_by', 'created_at')
+    search_fields = ('name', 'template_type')
+    list_filter = ('status', 'access_level')
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
 
-    def lookups(self, request, model_admin):
-        return [
-            ('<100', 'до 100 млрд'),
-            ('100-500', '100–500 млрд'),
-            ('>500', 'более 500 млрд'),
-        ]
 
-    def queryset(self, request, queryset):
-        def parse(value):
-            try:
-                return float(value.replace(',', '.'))
-            except:
-                return 0
+@admin.register(ReworkTask)
+class ReworkTaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version', 'status', 'created_by', 'created_at')
+    search_fields = ('name', 'version')
+    list_filter = ('status', 'priority',)
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
 
-        if self.value() == '<100':
-            return queryset.filter(revenue_for_last_year__lt='100')
-        elif self.value() == '100-500':
-            return queryset.filter(
-                revenue_for_last_year__gte='100',
-                revenue_for_last_year__lte='500'
+
+@admin.register(WorkPlan)
+class WorkPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'version', 'status', 'created_by', 'created_at')
+    search_fields = ('name', 'version')
+    list_filter = ('status', 'priority',)
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by')
+
+@admin.register(TechTask)
+class TechTaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'version', 'status', 'created_by', 'created_at', 'updated_at', 'editor'
+    )
+    search_fields = ('name', 'version')
+    list_filter = ('status', 'access_level')
+    readonly_fields = ('created_at', 'updated_at', 'created_by', 'updated_by', 'change_description')
+
+    fieldsets = (
+        ('Основная информация', {
+            'fields': (
+                'name', 'version', 'status',
             )
-        elif self.value() == '>500':
-            return queryset.filter(revenue_for_last_year__gt='500')
-        return queryset
-
-
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('name_of_company', 'revenue_for_last_year', 'length_of_electrical_network_km')
-    list_filter = ('name_of_company', 'revenue_for_last_year')  # Фильтры в правой части
-    list_filter = (RevenueRangeFilter,)
-    search_fields = ('name_of_company', 'address')  # Поиск по этим полям
-
-
-class Decision_makerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'city_of_location', 'function', 'customer')
-    list_filter = ('city_of_location', 'function', 'customer')
-    search_fields = ('full_name', 'phone_number', 'email')
-
-
-class DealAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'start_date', 'status', 'deal_amount')
-    list_filter = ('customer', 'start_date', 'customer')
-    search_fields = ('customer__name_of_company', 'description')
-    date_hierarchy = 'start_date'  # Иерархия по дате
-
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name_of_product', 'end_customer_price')
-    list_filter = ('name_of_product',)
-    search_fields = ('name_of_product', 'description')
-
-
-class Deal_stageAdmin(admin.ModelAdmin):
-    list_display = ('deal', 'start_date_step', 'status')
-    list_filter = ('status', 'deal')
-    search_fields = ('deal__customer__name_of_company', 'description_of_task_at_stage')
-
-
-@admin.register(Call)
-class CallAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'decision_maker', 'planned_date', 'responsible', 'deal')
-    search_fields = ('call_goal', 'call_result')
-    list_filter = ('planned_date',)
-    date_hierarchy = 'planned_date'
-
-
-@admin.register(Letter)
-class LetterAdmin(admin.ModelAdmin):
-    list_display = ('incoming_number', 'customer', 'planned_date', 'responsible', 'deal')
-    search_fields = ('incoming_number', 'responsible_person_from_customer')
-    list_filter = ('planned_date',)
-    date_hierarchy = 'planned_date'
-
-admin.site.register(Customer, CustomerAdmin)
-admin.site.register(Decision_maker, Decision_makerAdmin)
-admin.site.register(Deal, DealAdmin)
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Deal_stage, Deal_stageAdmin)
-admin.site.register(Notifications)
+        }),
+        ('Файловые и связные поля', {
+            'fields': (
+                'template', 'okr_task', 'rework_task', 'work_plan'
+            )
+        }),
+        ('Служебная информация', {
+            'fields': (
+                'created_by', 'created_at', 'updated_by', 'updated_at', 'editor',
+                'change_description', 'permission_level', 'access_level'
+            )
+        }),
+    )
