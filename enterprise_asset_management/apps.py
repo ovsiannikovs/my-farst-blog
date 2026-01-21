117641f66648ff9f2678058c727230cb6a7ac5aa
Nikita Karasev <n.d.karasev@yandex.ru>
2026-01-21 11:24:33 +0300

    EAM: add enterprise asset management app and work equipment admin

---
+++ enterprise_asset_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+class EnterpriseAssetManagementConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "enterprise_asset_management"
+    verbose_name = "EAM (СИСТЕМА УПРАВЛЕНИЕ АКТИВАМИ)"
